61c840dee7fa828f70a6d78925b624437d142329
Johnny Choi <idsopy@nate.com>
2022-08-11 23:40:31 +0900

    1

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-// Type of Data
+// // Type of Data
 
-let a = 3;
-let b = a;
+// let a = 3;
+// let b = a;
 
-let obj1 = { a: 1, b: 3};
-let obj2 = obj1;
+// let obj1 = { a: 1, b: 3};
+// let obj2 = obj1;
 
 
-let d = 5;
-let e = 5;
-d = 7;
+// let d = 5;
+// let e = 5;
+// d = 7;
 
-let obj3 = {a: 1, b: 4};
-obj3.a = 4;
-console.log(obj3);
+// let obj3 = {a: 1, b: 4};
+// obj3.a = 4;
+// console.log(obj3);
+
+function a () {
+    var b
+    function b () {};
+    console.log(b);
+    var b = 'bbb';
+    console.log(b);
+    function b () {};
+    console.log(b);    
+}
+
+a()
